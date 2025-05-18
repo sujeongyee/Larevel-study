53b49274b88ac47d7ecca41f3d9b6c1a43947e90
ddu <sujeongyee0529@gmail.com>
2025-05-18 17:31:42 +0900

    mass assignment

--- app/Models/Bike.php
+++ app/Models/Bike.php
@@ -8,5 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Bike extends Model
 {
     use HasFactory;
+    // 방법 1: $fillable (허용된 필드만 허용)
     protected $fillable = ['name', 'price', 'brand'];
+    // 방법 2: $guarded (막을 필드를 지정)
+    // protected $guarded = ['is_admin'];
 }
